16d7e91e1e0c805f874e1ee31f2e010bb3dd4768
ashishdixit628 <71096111+ashishdixit628@users.noreply.github.com>
2020-09-27 11:21:34 +0530

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main()
+{
+    int arr[10];
+    int n;
+    cin>>n
+    cout<<endl;
+    fr(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+}
