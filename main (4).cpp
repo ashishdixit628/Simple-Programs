d6f6c221216dc62e7c1743a7e5711fa5161008dc
ashishdixit628 <71096111+ashishdixit628@users.noreply.github.com>
2020-09-27 11:23:45 +0530

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+using namespace;  
+int main()  
+{  
+ int arr[5]={10, 0, 20, 0, 30} 
+ 
+        for (int i = 0; i < 5; i++)    
+        {    
+            cout<<arr[i]<<"\n";    
+        }    
+}  
