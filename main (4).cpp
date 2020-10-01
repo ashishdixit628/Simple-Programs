1193d4f1790b2e32d9badfc3f208c2077261bcd5
Abhishek Dixit <dabhishek957@gmail.com>
2020-10-01 14:44:30 +0530

    Update main (4).cpp

--- main (4).cpp
+++ main (4).cpp
@@ -2,11 +2,11 @@
 using namespace std;  
 int main()  
 {  
- int arr[5]={10, 0, 20, 0, 30};
+ int arr[5]={10, 0, 20, 0, 30};        //declaration of array
  
         for (int i = 0; i < 5; i++)    
         {    
-            cout<<arr[i]<<"\n";    
+            cout<<arr[i]<<"\n";    //output the array
         }    
  return 0;
 }  
