1387485e94b9a460a8256301c91b61bb129cd431
Abhishek Dixit <dabhishek957@gmail.com>
2020-10-01 14:33:55 +0530

    Update main (4).cpp

--- main (4).cpp
+++ main (4).cpp
@@ -1,11 +1,12 @@
 #include <iostream>  
-using namespace;  
+using namespace std;  
 int main()  
 {  
- int arr[5]={10, 0, 20, 0, 30} 
+ int arr[5]={10, 0, 20, 0, 30};
  
         for (int i = 0; i < 5; i++)    
         {    
             cout<<arr[i]<<"\n";    
         }    
+ return 0;
 }  
